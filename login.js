1bfe140b1e7e66957c78aa96e81fbc29c9deebf8
Son Truong <son@Sons-MacBook-Pro.local>
2015-06-26 23:30:13 -0700

    Schema kinda works

--- login.js
+++ login.js
@@ -1,4 +1,8 @@
 app.controller("LoginCtrl", function($scope, Auth, $location, $rootScope) {
+  var ref = new Firebase("https://shouting.firebaseio.com/");
+  var usersRef = ref.child("users")
+
+
   $scope.createUser = function() {
     $scope.message = null;
     $scope.error = null;
@@ -7,8 +11,9 @@ app.controller("LoginCtrl", function($scope, Auth, $location, $rootScope) {
       password: $scope.password
     })
     .then(function(userData) {
-      console.log("rootScope", $rootScope);
       $scope.message = "User created with uid: " + userData.uid;
+      var activeUserRef = usersRef.child(userData.uid)
+      activeUserRef.set({Followers: ["followers"], Name: $scope.name})
     })
     .catch(function(error) {
       console.log(error)
--- messageboard.js
+++ messageboard.js
@@ -1,6 +1,20 @@
 app.controller("MessageBoardCtrl", function($scope, Auth, $location, $rootScope) {
+  var ref = new Firebase("https://shouting.firebaseio.com/");
+  var usersRef = ref.child("users")
   $scope.logOut = function() {
     Auth.$unauth()
     $location.url("/")
+  }
+
+  $scope.submitTweet = function() {
+  	var currentUser = $rootScope.authData.uid;
+  	var currentUserRef = usersRef.child(currentUser)
+  	currentUserRef.child("Tweets").push($scope.tweet)
+
+  }
+  $scope.followUser = function() {
+  	var currentUser = $rootScope.authData.uid;
+  	var currentUserRef = usersRef.child(currentUser)
+  	currentUserRef.child("Following").push($scope.userToFollow)
   }
 })
